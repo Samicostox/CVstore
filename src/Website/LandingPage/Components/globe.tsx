6f2d7fd16e686d84882df406b9a301991b9d6c4e
uobjuniorsolutions <uonjuniorsolutions@gmail.com>
2024-04-08 13:59:23 +0100

    plausible

--- src/Website/LandingPage/Components/globe.tsx
+++ src/Website/LandingPage/Components/globe.tsx
@@ -261,7 +261,7 @@ export function World(props: WorldProps) {
     <Canvas
     style={{
       pointerEvents: isMobile ? 'none' : 'all',
-      marginTop: '40px'
+      marginTop: isMobile ? '70px' : '30px'
     }}
     scene={scene}
     camera={new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000)}
