4cd7a159d6636e56eab4d3a1b1251a9381e533fa
uobjuniorsolutions <uonjuniorsolutions@gmail.com>
2024-04-10 10:05:15 +0100

    a

--- src/Website/LandingPage/Components/cardsDispnay.js
+++ src/Website/LandingPage/Components/cardsDispnay.js
@@ -24,24 +24,25 @@ const ResponsiveCards = () => {
         downloadUrl: "https://res.cloudinary.com/dl2adjye7/raw/upload/v1712488977/Goldman-Sachs_furnlc.docx"
     },
     {status: "available", name: "BlackRock", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712481839/Untitled_design_24_te5qrr.png", description: "The world's largest asset manager, providing investment, advisory, and risk management solutions to institutional and retail clients globally.", downloadUrl: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712658790/blackrock_re5mnt.png" },
+    {status: "available", name: "Morgan Stanley", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313256/Untitled_design_19_lhg4xn.png", description: "A global financial services firm offering a wide range of investment banking, securities, wealth management, and investment management services.", downloadUrl: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712739859/CV_Morgan_Stanley_1_jmlmvd.pdf" },
     {status: "1 day ", name: "Amazon", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313049/Untitled_design_17_ptzwq9.png", description: "A global e-commerce and cloud computing giant, renowned for its technological innovation, extensive product offerings, and customer-focused services.", downloadUrl: "<Amazon_Download_URL>" },
     {status: "2 days", name: "Qube", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313160/Untitled_design_18_ozqsgm.png", description: "An innovative technology company specializing in digital transformation and business intelligence solutions, driving efficiency and growth for clients.", downloadUrl: "<Qube_Download_URL>" },
-    {status: "3 days", name: "Morgan Stanley", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313256/Untitled_design_19_lhg4xn.png", description: "A global financial services firm offering a wide range of investment banking, securities, wealth management, and investment management services.", downloadUrl: "<Morgan_Stanley_Download_URL>" },
-    {status: "4 days", name: "Barclays", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313333/Untitled_design_20_eeralu.png", description: "A British multinational bank and financial services company, offering products and services across personal, corporate, and investment banking.", downloadUrl: "<Barclays_Download_URL>" },
-    {status: "5 days", name: "Deloitte", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313412/Untitled_design_21_dr42ev.png", description: "One of the largest professional services networks in the world, providing audit, consulting, financial advisory, risk advisory, tax, and related services.", downloadUrl: "<Deloitte_Download_URL>" },
-    {status: "6 days", name: "Natwest", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313705/Untitled_design_23_vkcffq.png", description: "A major retail and commercial bank in the United Kingdom, known for its customer service, banking operations, and financial solutions.", downloadUrl: "<Natwest_Download_URL>" },
-    {status: "7 days", name: "Wise", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313625/Untitled_design_22_yn8tws.png", description: "A financial technology company offering innovative money transfer services, known for its transparent fees and efficient cross-border payments.", downloadUrl: "<Natwest_Download_URL>" },
+   
+    {status: "3 days", name: "Barclays", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313333/Untitled_design_20_eeralu.png", description: "A British multinational bank and financial services company, offering products and services across personal, corporate, and investment banking.", downloadUrl: "<Barclays_Download_URL>" },
+    {status: "4 days", name: "Deloitte", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313412/Untitled_design_21_dr42ev.png", description: "One of the largest professional services networks in the world, providing audit, consulting, financial advisory, risk advisory, tax, and related services.", downloadUrl: "<Deloitte_Download_URL>" },
+    {status: "5 days", name: "Natwest", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313705/Untitled_design_23_vkcffq.png", description: "A major retail and commercial bank in the United Kingdom, known for its customer service, banking operations, and financial solutions.", downloadUrl: "<Natwest_Download_URL>" },
+    {status: "6 days", name: "Wise", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712313625/Untitled_design_22_yn8tws.png", description: "A financial technology company offering innovative money transfer services, known for its transparent fees and efficient cross-border payments.", downloadUrl: "<Natwest_Download_URL>" },
     
-    {status: "8 days", name: "Bank of America", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712481990/Untitled_design_25_hgkfax.png", description: " A leading American multinational investment bank and financial services holding company, serving individual consumers.", downloadUrl: "<Natwest_Download_URL>" },
+    {status: "7 days", name: "Bank of America", image: "https://res.cloudinary.com/dl2adjye7/image/upload/v1712481990/Untitled_design_25_hgkfax.png", description: " A leading American multinational investment bank and financial services holding company, serving individual consumers.", downloadUrl: "<Natwest_Download_URL>" },
     {
-      "status": "9 days",
+      "status": "8 days",
       "name": "McKinsey",
       "image": "https://res.cloudinary.com/dl2adjye7/image/upload/v1712569143/Untitled_design_27_vdfo9b.png",
       "description": "A global management consulting firm that advises on strategic, operational, and organizational matters, serving a wide range of industries and public sector entities.",
       "downloadUrl": "<Natwest_Download_URL>"
   },
   {
-      "status": "10 days",
+      "status": "9 days",
       "name": "BCG",
       "image": "https://res.cloudinary.com/dl2adjye7/image/upload/v1712569251/bcg_fk645r.png",
       "description": "A worldwide management consulting firm providing advisory services in strategy, operations, organization, and digital transformation for various sectors.",
